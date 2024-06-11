f67ae969bee2400c44923444155feb95a2d265e5
natedevxyz <nate.d3v@gmail.com>
2024-06-10 22:17:49 -0400

    Removed unused files and directories

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
